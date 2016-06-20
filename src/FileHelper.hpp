b8b841ec78be6fe172386a205e8e95ca092b9b76
answer-general <answer.general@mail.ru>
2016-06-20 17:04:27 +0300

    Update FileHelper.hpp

--- src/FileHelper.hpp
+++ src/FileHelper.hpp
@@ -8,25 +8,26 @@
 #include <vector>
 /*!
  * \struct DirEntry
- * 
+ * Object in the directory.
  */
 struct DirEntry {
   /*!
    * \var std::string name
-   * 
+   * Name of the object
    * \public \memberof DirEntry
    */
   std::string name;
   /*!
    * \var bool isDir
-   * 
+   * Type of the object: file or directory.
    * \public \memberof DirEntry
    */
   bool isDir;
 };
 /*!
- * \var std::vector<DirEntry> listFiles(const std::string& dirPath)
- * 
+ * \fn std::vector<DirEntry> listFiles(const std::string& dirPath)
+ * Returns all files from directory, signing every object: file as a file, subdirectory as a directory.
+ * @param[in] dirPath Directory path.
  */
 std::vector<DirEntry> listFiles(const std::string& dirPath);
 
