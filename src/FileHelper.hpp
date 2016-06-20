a349458d8fdfa4fc3f04a505abc6a492e7ba349a
answer-general <answer.general@mail.ru>
2016-06-20 16:56:54 +0300

    Add comments

--- src/FileHelper.hpp
+++ src/FileHelper.hpp
@@ -1,14 +1,33 @@
+/**
+ * \file FileHelper.hpp
+ */
 #ifndef FILE_HELPER_HPP
 #define FILE_HELPER_HPP
 
 #include <string>
 #include <vector>
-
+/*!
+ * \struct DirEntry
+ * 
+ */
 struct DirEntry {
+  /*!
+   * \var std::string name
+   * 
+   * \public \memberof DirEntry
+   */
   std::string name;
+  /*!
+   * \var bool isDir
+   * 
+   * \public \memberof DirEntry
+   */
   bool isDir;
 };
-
+/*!
+ * \var std::vector<DirEntry> listFiles(const std::string& dirPath)
+ * 
+ */
 std::vector<DirEntry> listFiles(const std::string& dirPath);
 
 #endif
