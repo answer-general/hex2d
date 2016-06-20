cb5b6ec743ecbd3ebde26b58e8e9d9c216cc8330
Cap2 <general.answer@mail.ru>
2016-06-20 16:28:24 +0200

    More WIP in AI

--- src/AIInput.cpp
+++ src/AIInput.cpp
@@ -2,17 +2,23 @@
 #include "ActorMage.hpp"
 #include "Game.hpp"
 #include "ObjectContainer.hpp"
+#include "Level.hpp"
+#include <array>
 #include <vector>
 
+int Abs(int);
 int dist2(const Point& a, const Point& b);
 
 class AIInput::Private {
 public:
   static const int defaultRadius = 2;
+
   Private(Game& c, AIInput& s) : core(c), self(s),
       lastCommand(InputMethod::NoCommand),
       radius(defaultRadius) {};
+  Point currentPosition();
   Point findNearestTarget();
+  bool canMove(Point pos, enum InputMethod::Command direction);
 
   Game& core;
   AIInput& self;
@@ -20,6 +26,10 @@ public:
   InputMethod::Command lastCommand;
   int radius;
 };
+
+int Abs(int a) {
+  return (a >= 0) ? a : -1 * a;
+}
 
 int dist2(const Point& a, const Point& b) {
   int x2 = (a.x - b.x) * (a.x - b.x);
@@ -39,7 +49,26 @@ enum InputMethod::Command AIInput::getNextCommand() {
 }
 
 void AIInput::update() {
-  
+  Point curr = d->currentPosition();
+  Point tgt = d->findNearestTarget();
+
+  Point diff;
+  diff.x = Abs(tgt.x - curr.x);
+  diff.y = Abs(tgt.y - curr.y);
+
+  Point inc(0,0);
+  if ((tgt.x - curr.x) > 0)
+    inc.x = 1;
+  else if ((tgt.x - curr.x) < 0)
+    inc.x = -1;
+
+  if ((tgt.y - curr.y) > 0)
+    inc.y = 1;
+  else if ((tgt.y - curr.y) < 0)
+    inc.y = -1;
+
+  // Try optimal direction.
+//  if (diff.x < diff.y)
 }
 
 int AIInput::sightRadius() const {
@@ -52,13 +81,19 @@ void AIInput::setSightRadius(int r) {
   d->radius = r;
 }
 
-Point AIInput::Private::findNearestTarget() {
+Point AIInput::Private::currentPosition() {
   SPtr<ObjectContainer> objs = core.getObjects();
 
   SPtr<Actor> a = std::dynamic_pointer_cast<Actor>(
       objs->getObject(self.target));
 
-  Point pos = a->pos();
+  return a->pos();
+}
+
+Point AIInput::Private::findNearestTarget() {
+  SPtr<ObjectContainer> objs = core.getObjects();
+
+  Point pos = currentPosition();
 
   // Establish search region.
   Point topLeft = pos;
@@ -102,4 +137,31 @@ Point AIInput::Private::findNearestTarget() {
   }
 
   return res;
+}
+
+bool AIInput::Private::canMove(Point p, enum InputMethod::Command dir) {
+  const auto level = core.getLevel();
+
+  switch (dir) {
+  case InputMethod::MoveUp:
+    --p.y;
+    break;
+  case InputMethod::MoveDown:
+    ++p.y;
+    break;
+  case InputMethod::MoveLeft:
+    --p.x;
+    break;
+  case InputMethod::MoveRight:
+    ++p.x;
+    break;
+  default: // Not a move command.
+    return false;
+    break;
+  };
+
+  if (level->canCross(p))
+    return true;
+  else
+    return false;
 }
