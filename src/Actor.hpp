74bab36ee71b9e0c4e90eacae4d746f8d6e942e3
answer-general <answer.general@mail.ru>
2016-06-20 12:52:24 +0300

    Add comments

--- src/Actor.hpp
+++ src/Actor.hpp
@@ -6,20 +6,24 @@
 #include "InputMethod.hpp"
 
 class Game;
-
+//  Actor - base class for all game characters, including players & NPC's
 class Actor : public GameObject {
 public:
+  // Base constructor for characters' registration
   Actor(int id) : GameObject(id) {};
 
+  // Prohibit copying objects
   Actor(const Actor&) = delete;
   Actor& operator =(const Actor&) = delete;
-
+  
+  // Destructor
   virtual ~Actor() {
     // Free input.
     if (input)
       input->assign(GameObject::InvalidObject);
   };
-
+  
+  // Determine, which input method will be used to control the player's character
   virtual void setInputMethod(SPtr<InputMethod> in) { input = in; }
 
   // Bonus effects -- provide temporary boosts.
