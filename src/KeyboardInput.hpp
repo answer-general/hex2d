702ba0888a2b74a4306200ed2b88b0e646cbfb2b
answer-general <answer.general@mail.ru>
2016-06-20 19:05:38 +0300

    Add comments

--- src/KeyboardInput.hpp
+++ src/KeyboardInput.hpp
@@ -1,3 +1,6 @@
+/*!
+ * \file KeyboardInput.hpp
+ */
 #ifndef KEYBOARD_INPUT_HPP
 #define KEYBOARD_INPUT_HPP
 
@@ -5,18 +8,36 @@
 #include "InputMethod.hpp"
 
 class Game;
-
+/*!
+ * \class KeyboardInput
+ * Responisble for input from the keyboard.
+ */
 class KeyboardInput : public InputMethod {
 public:
+  /*!
+   * Base constructor.
+   * @param[in]  core Reference to the game core.
+   * \public \memberof KeyboardInput
+   */
   KeyboardInput(Game& core);
-
+  /*!
+   * Prohibit copying objects
+   */
   KeyboardInput(const KeyboardInput&) = delete;
   KeyboardInput& operator =(const KeyboardInput&) = delete;
 
   ~KeyboardInput();
-
+  /*!
+   * \fn enum InputMethod::Command getNextCommand()
+   * Gets next command.
+   * \public \memberof KeyboardInput
+   */
   enum InputMethod::Command getNextCommand();
-
+  /*!
+   * \fn void update()
+   * Update internal state.
+   * \public \memberof KeyboardInput
+   */
   void update();
 private:
   class Private;
